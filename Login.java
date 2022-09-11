e925439cfc23628ce13515ddfed2382fe0694e9b
Pedrowwww <pedrowwww44@gmail.com>
2022-09-11 15:20:14 -0300

    Mais uma vez

--- Login.java
+++ Login.java
@@ -1,4 +1,7 @@
+import java.awt.Font;
+
 import javax.swing.*;
+import Interfaces.Components.Butao;
 //import java.awt.*;
 public class Login {
     int telaHeight = 450, telaWidth = 400, textoHeight = 25, textoWidth = 200, gap = 30, paddinTop = 55;
@@ -6,6 +9,7 @@ public class Login {
         JFrame tela = new JFrame();
 
         JLabel titulo = new JLabel("LOGIN");
+        titulo.setFont(new Font("Arial", Font.BOLD, textoHeight));
         titulo.setBounds(telaWidth - 3*textoWidth/2, textoHeight+paddinTop, textoWidth, textoHeight);
         titulo.setHorizontalAlignment(JTextField.CENTER);
         
@@ -19,10 +23,10 @@ public class Login {
         JPasswordField senhaInput = new JPasswordField();
         senhaInput.setBounds(telaWidth - 3*textoWidth/2, textoHeight+gap*4+paddinTop, textoWidth, textoHeight);
 
-        JButton enviar = new JButton("ENVIAR");
+        JButton enviar = new Butao("ENVIAR");
         enviar.setBounds(telaWidth - 3*textoWidth/2, textoHeight+gap*5+paddinTop, textoWidth, textoHeight);
 
-        JButton cadastrar = new JButton("CADASTRAR");
+        JButton cadastrar = new Butao("CADASTRE-SE");
         cadastrar.setBounds(telaWidth - 3*textoWidth/2, textoHeight+gap*6+paddinTop, textoWidth, textoHeight);
 
         tela.add(titulo);
@@ -37,5 +41,6 @@ public class Login {
         tela.setResizable(false);
         tela.setLayout(null);
         tela.setVisible(true);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
